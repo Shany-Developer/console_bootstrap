710b346ae312bdc6ab05347e68c0020429c3d829
Shany Thankachan <Shany.Thankachan@skywalkglobal.net>
2022-03-18 21:37:49 +0530

    Refer nuget and one line code

--- console_bootstrap.Sample/Program.cs
+++ console_bootstrap.Sample/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using console_bootstrap;
+
+try
+{
+    var name = $"Please enter your name".ask();
+}
+catch (Exception ex)
+{
+    ex.error();
+}
+
